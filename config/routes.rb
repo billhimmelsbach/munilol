dff2852ec973610fdf90e901b3c19d6bd76de4b5
billhimmelsbach <whimmels@gmail.com>
2016-08-26 10:07:26 -0700

    fixes route for new user form

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   get "/login", to: "sessions#new", as: "new_session"
   get "/logout", to: "sessions#destroy"
-  post "/sessions", to: "sessions#create"
+  post "/sessions", to: "sessions#create", as: "create_session"
 
   get "/munis/:id", to: "munis#show", as: "muni"
 
