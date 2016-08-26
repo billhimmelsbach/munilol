6e8239c04f4cff11f288ee121a743496ba80e783
billhimmelsbach <whimmels@gmail.com>
2016-08-25 18:32:20 -0700

    adds initial routes for users, sessions, munis, articles, comments, and unmatched routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
-  
+  root to: "articles#index"
+
+  get "/users/new", to: "users#new", as: "new_user"
+  post "/users", to: "users#create"
+  get "/users/:id", to: "users#show", as: "user"
+  get "/users/:id/edit", to: "users#edit", as: "edit_user"
+  patch "/users/:id", to: "users#update"
+  delete "/users/:id", to: "users#destroy", as: "delete_user"
+
+  get "/login", to: "sessions#new", as: "new_session"
+  get "/logout", to: "sessions#destroy"
+  post "/sessions", to: "sessions#create"
+
+  get "/munis/:id", to: "munis#show", as: "muni"
+
+  get "/articles/new", to: "articles#new", as: "new_article"
+  post "/articles", to: "articles#create"
+  get "/articles/:id", to: "articles#show", as: "article"
+  get "/articles/:id/edit", to: "articles#edit", as: "edit_article"
+  patch "/articles/:article_id", to: "articles#update"
+  delete "/articles/:id", to: "articles#destroy", as: "delete_article"
+
+  patch "/comments/:id", to: "comments#update"
+
+  get "*unmatched_route", to: "application#not_found"
 end
