23fc6ed7859563ca56e4fab8a307c852d81ac7f7
billhimmelsbach <whimmels@gmail.com>
2016-08-25 22:17:08 -0700

    finishes implementing bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   #TODO set default user picture
+  has_secure_password
+
+  def self.confirm(params)
+    @user = User.where("email ILIKE ?", params[:email]).first
+    @user.try(:authenticate, params[:password])
+  end
 end
