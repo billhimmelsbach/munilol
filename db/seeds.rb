13551baeb8ad383f8a034201e3e36a8b144ec5cf
billhimmelsbach <whimmels@gmail.com>
2016-08-26 08:34:06 -0700

    removes password digest from seeds that don't have seeded passwords yet, since it prevents generating ids, created_at, etc

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,28 +3,28 @@ USERS = [
     first_name: "Nathan",
     last_name: "Allen",
     email: "NathanAllen@WDI30.com",
-    password_digest:  "",
+    password_digest: "$2a$10$ZOznVN9PnbO2eww2ARycPu.P4nFJ092edGENVpofzmM6ISZrzzGM.",
     image: "https://cdn0.vox-cdn.com/images/verge/default-avatar.v9899025.gif"
   },
   {
     first_name: "Justin",
     last_name: "Castilla",
     email: "JustinCastilla@WDI30.com",
-    password_digest:  "",
+    # password_digest:  "",
     image: "https://cdn0.vox-cdn.com/images/verge/default-avatar.v9899025.gif"
   },
   {
     first_name: "Ben",
     last_name: "Manning",
     email: "BenManning@WDI30.com",
-    password_digest:  "",
+    # password_digest:  "",
     image: "https://cdn0.vox-cdn.com/images/verge/default-avatar.v9899025.gif"
   },
   {
     first_name: "Melissa",
     last_name: "Carlson",
     email: "MelissaCarlson@WDI30.com",
-    password_digest:  "",
+    # password_digest:  "",
     image: "https://cdn0.vox-cdn.com/images/verge/default-avatar.v9899025.gif"
   }
 ]
@@ -60,21 +60,21 @@ ARTICLES = [
     title: "Roger Rabbit takes the 19-Polk to Toontown",
     content: "I mean, there is a lot of boozing in the movie, so the 19 is probably the perfect route to ride.",
     image: "http://www.munidiaries.com/wp-content/uploads/2016/05/roger_rabbit2.jpg",
-    user_id: 1,
+    user_id: 2,
     muni_id: 1,
   },
   {
     title: "Poop-cleaning neighbors still need Muni’s help",
     content: "The management at the apartment building at Post and Leavenworth has gone above and beyond in keeping their block nice and not-too-smelly, extending even to the Muni stop in front of the building. But when the Muni stop poop bandit hits your block, it’s just all too much. Here is their PSA sign that doubles as a cry for help.",
     image: "http://www.munidiaries.com/wp-content/uploads/2016/05/muni-poop-cleaning.jpg",
-    user_id: 1,
+    user_id: 3,
     muni_id: 1,
   },
   {
     title: "Skater eschews pushing, hitches a ride ‘on’ Muni",
     content: "We’ve seen this type of heroics activity before. You know the saying: If Marty McFly can do it …",
     image: "http://www.munidiaries.com/wp-content/uploads/2016/02/skater.jpg",
-    user_id: 1,
+    user_id: 4,
     muni_id: 1,
   }
 ]
@@ -99,11 +99,11 @@ COMMENTS = [
     vote: 1,
     user_id: 4,
     article_id: 1
-  },
+  }
 ]
 
 
-User.create(USERS)
-Muni.create(MUNIS)
-Article.create(ARTICLES)
-Comment.create(COMMENTS)
+p User.create(USERS)
+p Muni.create(MUNIS)
+p Article.create(ARTICLES)
+p Comment.create(COMMENTS)
