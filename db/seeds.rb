8bd0890964d69e1c226d5146748e9cb37c843646
WDIDavidKim <davidjkim1984@gmail.com>
2016-08-25 19:56:55 -0700

    added seed data and created a footer

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,72 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+USERS = [
+{
+  first_name: "Nathan",
+  last_name: "Allen",
+  email: "NathanAllen@WDI30.com",
+  password_digest:  "",
+  image: "https://cdn0.vox-cdn.com/images/verge/default-avatar.v9899025.gif",
+},
+{
+  first_name: "Justin",
+  last_name: "Castilla",
+  email: "JustinCastilla@WDI30.com",
+  password_digest:  "",
+  photo: "https://cdn0.vox-cdn.com/images/verge/default-avatar.v9899025.gif",
+},
+{
+  first_name: "Ben",
+  last_name: "Manning",
+  email: "BenManning@WDI30.com",
+  password_digest:  "",
+  photo: "https://cdn0.vox-cdn.com/images/verge/default-avatar.v9899025.gif",
+},
+{
+  first_name: "Melissa",
+  last_name: "Carlson",
+  email: "MelissaCarlson@WDI30.com",
+  password_digest:  "",
+  photo: "https://cdn0.vox-cdn.com/images/verge/default-avatar.v9899025.gif",
+}
+]
+
+MUNIS = [
+  {
+    name: "1-California",
+    description: "ipsem Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    name: "25-Treasure Island",
+    description: "ipsem Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    name: "38-Geary",
+    description: "ipsem Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    name: "47-Van Ness",
+    description: "ipsem Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  }
+]
+
+ARTICLES = [
+  {
+    title: "Crow-M-G!!! Bird flies into Muni bus",
+    content: "Muni rider Cynthia had a rather unexpected fellow passenger over the weekend.",
+    image: "http://www.munidiaries.com/wp-content/uploads/2016/08/crow.jpg"
+  },
+  {
+    title: "Roger Rabbit takes the 19-Polk to Toontown",
+    content: "I mean, there is a lot of boozing in the movie, so the 19 is probably the perfect route to ride.",
+    image: "http://www.munidiaries.com/wp-content/uploads/2016/05/roger_rabbit2.jpg"
+  },
+  {
+    title: "Poop-cleaning neighbors still need Muni’s help",
+    content: "The management at the apartment building at Post and Leavenworth has gone above and beyond in keeping their block nice and not-too-smelly, extending even to the Muni stop in front of the building. But when the Muni stop poop bandit hits your block, it’s just all too much. Here is their PSA sign that doubles as a cry for help.",
+    image: "http://www.munidiaries.com/wp-content/uploads/2016/05/muni-poop-cleaning.jpg"
+  },
+  {
+    title: "Skater eschews pushing, hitches a ride ‘on’ Muni",
+    content: "We’ve seen this type of heroics activity before. You know the saying: If Marty McFly can do it …",
+    image: "http://www.munidiaries.com/wp-content/uploads/2016/02/skater.jpg"
+  }
+]
