753b7a6c2c6f3f41ed1d21568bae2156c8436351
billhimmelsbach <whimmels@gmail.com>
2016-08-26 20:11:48 -0700

    hotfix for default user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ActiveRecord::Base
   #TODO set default user picture
   has_secure_password
 
+
+  before_create do
+    set_default_image
+  end
+
   # extend FriendlyId
   # friendly_id :first_name, use: [:slugged, :finders]
 
@@ -13,11 +18,10 @@ class User < ActiveRecord::Base
   # validates :first_name, length: { in: 2..30}, presence: true
   # validates :last_name, length: { in: 2..30}, presence: true
 
-
   private
-
+  
   def set_default_image
-    self.image ||= "https://cdn0.vox-cdn.com/images/verge/default-avatar.v9899025.gif"
+    self.image = "https://cdn0.vox-cdn.com/images/verge/default-avatar.v9899025.gif" if self.image=""
   end
 
   def self.confirm(params)
