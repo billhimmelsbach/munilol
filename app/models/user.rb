f668ccfd032257889df12fa1c55659ac6bf245d5
billhimmelsbach <whimmels@gmail.com>
2016-08-31 15:31:11 -0700

    fixes a lingering bug with friendly id, moves setting default values to before validation instead of before create since friendly id runs at the before validation step

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
   #TODO set default user picture
   has_secure_password
-
   extend FriendlyId
+
+  before_validation :default_values
+
+
   friendly_id :full_name, use: [:slugged, :finders]
 
 
@@ -17,13 +20,11 @@ class User < ActiveRecord::Base
   has_many :munis, :through => :articles
   has_many :comments, :through => :articles
 
-  before_save :default_values
-
   private
 
   def default_values
     self.image = "https://ucarecdn.com/1386c488-f2db-4b63-959f-32656a7e35c6/" if self.image == ""
-    self.full_name = "#{self.first_name + self.last_name}"
+    self.full_name = "#{self.first_name} #{self.last_name}"
   end
 
 
@@ -31,4 +32,9 @@ class User < ActiveRecord::Base
     @user = User.where("email ILIKE ?", params[:email]).first
     @user.try(:authenticate, params[:password])
   end
+
+  def should_generate_new_friendly_id?
+    slug.blank? || username_changed?
+  end
+
 end
