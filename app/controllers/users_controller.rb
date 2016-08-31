921f3b6a64817173ef16703817d898b79afd2297
billhimmelsbach <whimmels@gmail.com>
2016-08-31 13:14:09 -0700

    fixes users friendlyurl, uses a new column full_name to display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,7 +61,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :image)
+    params.require(:user).permit(:first_name, :last_name, :full_name, :email, :password, :image)
   end
 
   def set_user
